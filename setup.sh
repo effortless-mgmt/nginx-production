359c05b40089eefe84c08f48a1439384895d0ee3
algorythm <anders@wiberg.tech>
2018-09-14 15:17:36 +0200

    Only download nginx.tmpl if it doesn't exist

--- setup.sh
+++ setup.sh
@@ -3,23 +3,22 @@
 ### Guide used: https://blog.ssdnodes.com/blog/host-multiple-ssl-websites-docker-nginx/
 
 echo "Docker network: nginx-proxy"
-
 if [ ! "$(docker network ls | grep nginx-proxy)" ]; then
     docker network create nginx-proxy
 else    
     echo "exist!"
 fi
 
-echo "Downloading nginx.tmpl"
-
-curl https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl > nginx.tmpl
+if ! -f nginx.tmpl 
+then
+    echo "Downloading nginx.tmpl"
+    curl https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl > nginx.tmpl
+fi
 
 echo "Starting nginx-proxy"
-
 docker-compose up -d
 
 echo "Nginx should be started. Manually check it below. There should be 3 containers respectively named 'nginx-proxy', 'nginx-proxy-gen' and 'nginx-proxy-le':"
-
 docker ps | grep nginx-proxy
 
 # If they don't work, use docker logs nginx-proxy, nginx-proxy-gen, nginx-proxy-le to see what is wrong.
